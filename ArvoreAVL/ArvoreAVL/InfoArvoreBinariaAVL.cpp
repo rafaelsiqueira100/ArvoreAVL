fa76e0a3f4dcb27e2be541a107626c63b66913a1
rafaelsiqueira100 <rafaelsiqueira100@gmail.com>
2018-09-14 11:46:25 -0300

    Criadas classes básicas

---
+++ ArvoreAVL/ArvoreAVL/InfoArvoreBinariaAVL.cpp
@@ -0,0 +1,16 @@
+
+#include "InfoArvoreBinariaAVL.h"
+
+
+InfoArvoreEnaria::InfoArvoreEnaria()
+{
+}
+
+
+InfoArvoreEnaria::~InfoArvoreEnaria()
+{
+}
+
+ostream& operator<< (ostream& os, InfoArvoreEnaria& info) throw() {
+	return((info).print(os));
+}
